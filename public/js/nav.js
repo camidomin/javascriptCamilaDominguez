65c0a5995f35ba4d5a100ba6da5c5a4d7eca3b68
Cami Dominguez <camiladominguez@MacBook-Air-de-Cami.local>
2022-05-10 16:01:02 -0300

    first commit

---
+++ public/js/nav.js
@@ -0,0 +1,103 @@
+// navbar
+
+const navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', () => {
+    if(scrollY >= 180){
+        navbar.classList.add('bg');
+    } else{
+        navbar.classList.remove('bg');
+    }
+})
+
+const createNavbar = () => {
+    let navbar = document.querySelector('.navbar');
+
+    navbar.innerHTML += `
+    
+        <ul class="links-container">
+        <li class="link-item"><a href="../public/index.html" class="link active">home</a></li>
+            <li class="link-item"><a href="../public/product.html" class="link">product</a></li>
+            <li class="link-item"><a href="../public/seller.html" class="link">seller</a></li>
+            <li class="link-item"><a href="../public/dashboard.html" class="link">dashboard</a></li>
+        </ul>
+        <div class="user-interactions">
+        <div class="search-box">
+            <input type="text" class="search" placeholder="search item">
+            <button class="search-btn"><img src="../public/img/search.png" alt=""></button>
+        </div>
+            <div class="cart" onclick="location.href='/cart'">
+            <img src="../public/img/cart.png" class="cart-icon" alt="">
+            <span class="cart-item-count">00</span>
+            
+            </div>
+            <div class="user">
+                <img src="../public/img/user.png" class="user-icon" alt="">
+                <div class="user-icon-popup">
+                    <p>login to your account</p>
+                    <a>login</a>
+                </div>
+            </div>
+        </div>
+
+    `
+}
+
+createNavbar();
+
+// user icon popup
+
+let userIcon = document.querySelector('.user-icon');
+let userPopupIcon = document.querySelector('.user-icon-popup');
+
+userIcon.addEventListener('click', () => userPopupIcon.classList.toggle('active'))
+
+let text = userPopupIcon.querySelector('p');
+let actionBtn = userPopupIcon.querySelector('a');
+let user = JSON.parse(sessionStorage.user || null);
+
+if(user != null){ // user is logged in
+    text.innerHTML = `log in as, ${user.name}`;
+    actionBtn.innerHTML = 'log out';
+    actionBtn.addEventListener('click', () => logout());
+} else{
+    text.innerHTML = 'login to your account';
+    actionBtn.innerHTML = 'login';
+    actionBtn.addEventListener('click', () => location.href = '/login');
+}
+
+const logout = () => {
+    sessionStorage.clear()
+    location.reload();
+}
+
+// search box
+
+let searchBtn = document.querySelector('.search-btn')
+let searchBox = document.querySelector('.search');
+
+searchBtn.addEventListener('click', () => {
+    if(searchBox.value.length){
+        location.href = `/search/${searchBox.value}`;
+    }
+})
+
+// nav cart count
+
+const updateNavCartCounter = () => {
+    let cartCounter = document.querySelector('.cart-item-count');
+
+    let cartItem = JSON.parse(localStorage.getItem('cart'));
+
+    if(cartItem == null){
+        cartCounter.innerHTML = '00';
+    } else{
+        if(cartItem.length > 9){
+            cartCounter.innerHTML = '9+';
+        } else if(cartItem.length <= 9){
+            cartCounter.innerHTML = `0${cartItem.length}`
+        }
+    }
+}
+
+updateNavCartCounter();
